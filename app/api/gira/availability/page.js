9053e88cacc4091c1875ec132b1017d85ebf0952
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-07-07 14:16:46 +0100

    corrected error, disabling api/gira/availability

--- app/api/gira/availability/page.js
+++ app/api/gira/availability/page.js
@@ -1,15 +1,17 @@
 //
 // alterantivamente,  importei aos dados JSON. mas nao funciona. 
-// mesmo apagando as linhas 10 e 11 ele acede 
 // a API mas nao faz nada, nao retorna. 
 
-// import bikeLanesGeoJSON from '/public/gira.json'
+import bikeLanesGeoJSON from '/public/gira.json'
 
 export default async function handler(req, res) {
   try {
 
-    const apiResponse = await fetch('https://opendata.emel.pt/cycling/gira/station/availability'); // Replace with the actual API endpoint
-    const bikeLanesGeoJSON = await apiResponse.json();
+    //
+    // nao esta a funcionar...
+    
+    // const apiResponse = await fetch('https://opendata.emel.pt/cycling/gira/station/availability'); // Replace with the actual API endpoint
+    // const bikeLanesGeoJSON = await apiResponse.json();
 
     res.status(200).json(bikeLanesGeoJSON);
   } catch (error) {
