a08f88c868f82edcd3ffafd1c00bca4dd8ca62e5
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-07-07 14:18:25 +0100

    corrected error, disabling api/gira/availability

--- app/api/gira/availability/page.js
+++ app/api/gira/availability/page.js
@@ -9,14 +9,14 @@ export default async function handler(req, res) {
 
     //
     // nao esta a funcionar...
-    
+
     // const apiResponse = await fetch('https://opendata.emel.pt/cycling/gira/station/availability'); // Replace with the actual API endpoint
     // const bikeLanesGeoJSON = await apiResponse.json();
 
-    res.status(200).json(bikeLanesGeoJSON);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ message: 'Error fetching data from API' });
-  }
+  //   res.status(200).json(bikeLanesGeoJSON);
+  // } catch (error) {
+  //   console.error('Error fetching data:', error);
+  //   res.status(500).json({ message: 'Error fetching data from API' });
+  // }
 }
 
