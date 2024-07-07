1674c5ba90c8bb1d74a78d75f70e434f6fe800da
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-07-07 14:21:31 +0100

    removed route access to API

--- app/api/gira/availability/page.js
+++ app/api/gira/availability/page.js
@@ -6,17 +6,17 @@ import bikeLanesGeoJSON from '/public/gira.json'
 
 export default async function handler(req, res) {
   try {
-
+    
     //
     // nao esta a funcionar...
 
     // const apiResponse = await fetch('https://opendata.emel.pt/cycling/gira/station/availability'); // Replace with the actual API endpoint
     // const bikeLanesGeoJSON = await apiResponse.json();
 
-  //   res.status(200).json(bikeLanesGeoJSON);
-  // } catch (error) {
-  //   console.error('Error fetching data:', error);
-  //   res.status(500).json({ message: 'Error fetching data from API' });
-  // }
+    res.status(200).json(bikeLanesGeoJSON);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ message: 'Error fetching data from API' });
+  }
 }
 
